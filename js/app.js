bba3854e6470abe0b3ab3d2ab3127172f675890a
Denis <www_gef_12@mail.ru>
2018-12-28 12:12:37 +0200

    create

---
+++ js/app.js
@@ -0,0 +1,90 @@
+'use strict';
+
+/*
+ * A Design by GraphBerry
+ * Author: GraphBerry
+ * Author URL: http://graphberry.com
+ * License: http://graphberry.com/pages/license
+ */
+ jQuery(document).ready(function ($) {
+
+    var lastId,
+    topMenu = $("#top-navigation"),
+    topMenuHeight = topMenu.outerHeight(),
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function () {
+            var href = $(this).attr("href");
+            if(href.indexOf("#") === 0){
+                var item = $($(this).attr("href"));
+                if (item.length) {
+                    return item;
+                }
+            }
+        });
+   
+    $(window).load(function () {
+        function filterPath(string) {
+            return string.replace(/^\//, '').replace(/(index|default).[a-zA-Z]{3,4}$/, '').replace(/\/$/, '');
+        }
+        $('a.more').each(function () {
+            if (filterPath(location.pathname) == filterPath(this.pathname) && location.hostname == this.hostname && this.hash.replace(/#/, '')) {
+                var $targetId = $(this.hash),
+                $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+                var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+
+                if ($target) {
+
+                    $(this).click(function () {
+
+                        //Hack collapse top navigation after clicking
+                        topMenu.parent().attr('style', 'height:0px').removeClass('in'); //Close navigation
+                        $('.navbar .btn-navbar').addClass('collapsed');
+
+                        var targetOffset = $target.offset().top - 111;
+                        $('html, body').animate({
+                            scrollTop: targetOffset
+                        }, 600);
+                        return false;
+                    });
+                }
+            }
+        });
+	});
+
+    
+
+    //Function for show or hide portfolio desctiption.
+    $.fn.showHide = function (options) {
+        var defaults = {
+            speed: 1000,
+            easing: '',
+            changeText: 0,
+            showText: 'Show',
+            hideText: 'Hide'
+        };
+        var options = $.extend(defaults, options);
+        $(this).click(function () {
+            $('.toggleDiv').slideUp(options.speed, options.easing);
+            var toggleClick = $(this);
+            var toggleDiv = $(this).attr('rel');
+            $(toggleDiv).slideToggle(options.speed, options.easing, function () {
+                if (options.changeText == 1) {
+                    $(toggleDiv).is(":visible") ? toggleClick.text(options.hideText) : toggleClick.text(options.showText);
+                }
+            });
+            return false;
+        });
+    };
+
+    //Initial Show/Hide portfolio element.
+    $('div.toggleDiv').hide();
+    $('.show_hide').showHide({
+        speed: 500,
+        changeText: 0,
+        showText: 'View',
+        hideText: 'Close'
+    });
+    
+});
